0f2caf8b2623ae7760e09859a87355f5c86a6854
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2023-06-16 20:26:09 +0200

    Update crea_grafo.c

--- Graph_Algorithms/Create_Graph/crea_grafo.c
+++ Graph_Algorithms/Create_Graph/crea_grafo.c
@@ -1,122 +1,101 @@
 /*
-* @author Elia Renzoni
-* @date 27/05/2023
-* @brief creazione di un grafo e visita in ampiezza
+*  @author Elia Renzoni
+*  @date 16/06/2023
+*  @brief algoritmo per la creazione di un Grafo - Liste di Adiacenza
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define LIMITE_MAX 50
-#define NOP        -1
+#define NOP -1
 
-typedef enum {
-  bianco, nero, grigio
-} colore_t;
-
-typedef struct vertici {
-  int chiave, distanza;
+typedef struct veritici {
+  int chiave;
   struct vertici *vertice_succ;
-  struct archi *archi_succ;
-  struct vertici *padre;
+  struct archi *testa_lista;
 } vertici_t;
 
 typedef struct archi {
-  struct vertici *vertice_adj;
+  struct vertici *vertice;
   struct archi *arco_succ;
 } archi_t;
 
 void creazione_lista_primaria(vertici_t **, int);
-int controllo_valori_lista_sec(vertici_t *);
 void creazione_lista_secondaria(archi_t **, int);
+int cerca_valore_corretto(vertici_t *, int);
+void visita_grafo_naive(vertici_t *);
 
-int main(void) {
-  vertici_t *testa_lista_p = NULL;
-  vertici_t *elem_lista_p = NULL;
-  archi_t *testa_lista_s = NULL;
-  int valori, numero_elem_p, numero_elem_s, contatore;
+int main(int argc, char** argv) {
+  vertici_t *testa_lista_primaria = NULL;
+  vertici_t *vertice;
+  archi_t *testa_lista_secondaria = NULL;
+  int valore, numero_vertici, numero_archi_uscenti, contatore;
 
-  do {
-    printf("Inserisci il numero dei vertici > \n");
-    scanf("%d", &numero_elem_p);
-    if (numero_elem_p < 1)
-      printf("Errore !\n");
-    while (getchar() != '\n');
-  } while (numero_elem_p < 1);
-
+  printf("Inserisci il numero di vertici > \n");
+  scanf("%d", &numero_vertici);
   srand(time(NULL));
-  for (contatore = 0; (contatore < numero_elem_p); contatore++) {
-    valori = rand() % LIMITE_MAX;
-    creazione_lista_primaria(&testa_lista_p, valori);
+  for (contatore = 0; (contatore < numero_vertici); contatore++) {
+    valore = rand() % 50;
+    creazione_lista_primaria(&testa_lista_primaria, valore);
   }
-  for (elem_lista_p = testa_lista_p; (elem_lista_p != NULL); elem_lista_p = elem_lista_p->vertice_succ) {
-    do {
-      printf("Inserisci il numero degli archi uscenti >\n");
-      scanf("%d", &numero_elem_s);
-      if (numero_elem_s < 0)
-        printf("Errore !\n");
-      while (getchar() != '\n');
-    } while (numero_elem_s < 0);
-    for (contatore = 0; (contatore < numero_elem_s); contatore++) {
-      valori = controllo_valori_lista_sec(testa_lista_p);
-      creazione_lista_secondaria(&testa_lista_s, valori);
-      elem_lista_p->archi_succ = testa_lista_s;
+  for (vertice = testa_lista_primaria; (vertice != NULL); vertice = vertice->vertice_succ) {
+    printf("Inserisci il numero degli archi uscenti per vertice > \n");
+    scanf("%d", &numero_archi_uscenti);
+
+    for (contatore = 0; (contatore < numero_archi_uscenti); contatore++) {
+      valore = rand() % 50;
+      if (cerca_valore_corretto(testa_lista_primaria) != NOP) {
+        creazione_lista_secondaria(&testa_lista_secondaria, valore);
+      }
+      vertice->testa_lista = testa_lista_secondaria;
     }
-    testa_lista_s = NULL;
   }
+  visita_grafo_naive(testa_lista_primaria);
   return (0);
 }
 
-void creazione_lista_primaria(vertici_t **testa_lista_p, int valore_vertice) {
-  vertici_t *veritice_nuovo, *vertice_gen;
-
-  for (vertice_gen = *testa_lista_p;
-        (vertice_gen != NULL && vertice_gen->chiave != valore_vertice);
-       vertice_gen = vertice_gen->vertice_succ);
+void creazione_lista_primaria(vertici_t **testa_lista, int valore) {
+  vertici_t *nuovo_vertice, *vertice_gen;
+  for (vertice_gen = *testa_lista; (vertice_gen != NULL); vertice_gen = vertice_gen->vertice_succ);
   if (vertice_gen == NULL) {
-    veritice_nuovo = (vertici_t *) malloc(sizeof(vertici_t));
-    veritice_nuovo->chiave = valore_vertice;
-    veritice_nuovo->padre = NULL;
-    veritice_nuovo->archi_succ = NULL;
-    veritice_nuovo->vertice_succ = NULL;
-    if (vertice_gen == *testa_lista_p)
-      *testa_lista_p = veritice_nuovo;
+    nuovo_vertice = (vertici_t *)malloc(sizeof(vertici_t));
+    nuovo_vertice->chiave = valore;
+    nuovo_vertice->vertice_succ = NULL;
+    if (vertice_gen == *testa_lista)
+      *testa_lista = nuovo_vertice;
     else
-      vertice_gen->vertice_succ = veritice_nuovo;
+      vertice_gen->vertice_succ = nuovo_vertice;
   }
 }
 
-int controllo_valori_lista_sec(vertici_t *testa_lista) {
-  int valore_gen = rand() % LIMITE_MAX;
-  int verifica_valore;
-  vertici_t *vertice_gen;
-
-  for (vertice_gen = testa_lista; (vertice_gen != NULL); vertice_gen = vertice_gen->vertice_succ) {
-    if (vertice_gen->chiave == valore_gen)
-      verifica_valore = 0;
-    else
-      verifica_valore = 1;
-  }
-  return ((verifica_valore == 1)? valore_gen : NOP);
+int cerca_valore_corretto(vertici_t *vertici, int val_cerc) {
+  vertici_t *elem;
+  for (elem = vertici; (elem != NULL && elem->chiave != val_cerc); elem = elem->vertice_succ) 
+  return ((elem != NULL)? 1 : 0);
 }
 
-void creazione_lista_secondaria(archi_t **testa_lista_s, int valore_uscente) {
-  archi_t *arco_uscente, *nuovo_arco;
-
-  if (valore_uscente != NOP) {
-    for (arco_uscente = *testa_lista_s;
-          (arco_uscente != NULL);
-        arco_uscente = arco_uscente->arco_succ);
-    if (arco_uscente == NULL) {
-      nuovo_arco = (archi_t *) malloc(sizeof(archi_t));
-      nuovo_arco->arco_succ = NULL;
-      nuovo_arco->vertice_adj->chiave = valore_uscente;
-      if (arco_uscente == *testa_lista_s)
-        *testa_lista_s = nuovo_arco;
-      else
-        arco_uscente->arco_succ = nuovo_arco;
-    }
+void creazione_lista_secondaria(archi_t **testa_lista, int valore) {
+  archi_t *arco_gen, *nuovo_arco;
+  for (arco_gen = *testa_lista; (arco_gen != NULL); arco_gen = arco_gen->arco_succ);
+  if (arco_gen == NULL) {
+    nuovo_arco = (archi_t *)malloc(sizeof(archi_t));
+    nuovo_arco->vertice->chiave = valore;
+    nuovo_arco->arco_succ = NULL;
+    if (arco_gen == *testa_lista)
+      *testa_lista = nuovo_arco;
+    else
+      arco_gen->arco_succ = nuovo_arco;
   }
+}
 
+void visita_grafo_naive(vertici_t *testa_lista) {
+  vertici_t *elem;
+  archi_t *arco;
+  for (elem = testa_lista; (elem != NULL); elem = elem->vertice_succ) {
+    printf("%d \t", elem->chiave);
+    for (arco = elem->testa_lista; (arco != NULL); arco = arco->arco_succ)
+      printf("%d \t", arco->vertice->chiave);
+    putchar("\n");
+  }
 }
